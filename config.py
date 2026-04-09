9db21b9d85950d4cf4e8d353ef00fe5088fefb65
JoseTbn <josebatumeni@gmail.com>
2026-04-09 13:20:48 +0200

    feat: scaffold vape shop bot project

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+ADMIN_CHAT_ID = int(os.environ["ADMIN_CHAT_ID"])
+PAYPAL_LINK = os.environ["PAYPAL_LINK"]
+SHOP_NAME = os.environ.get("SHOP_NAME", "Vape Shop")
+SHOP_INFO = os.environ.get("SHOP_INFO", "Premium vaping products.")
+CONTACT_INFO = os.environ.get("CONTACT_INFO", "Contact us via Telegram.")
+CATALOG_PATH = os.environ.get("CATALOG_PATH", "catalog.json")
+ORDERS_PATH = os.environ.get("ORDERS_PATH", "orders.json")
